a537c1346d6918a7dae459ae30d7161d5a84b554
marciogrecco <marcio.greco@outlook.com>
2021-12-07 19:48:48 -0300

    created instance

--- src/main/java/com/backend/application/ApiBackendJavaApplication.java
+++ src/main/java/com/backend/application/ApiBackendJavaApplication.java
@@ -1,13 +1,35 @@
 package com.backend.application;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiBackendJavaApplication {
+import com.backend.application.domain.Categoria;
+import com.backend.application.domain.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class ApiBackendJavaApplication implements CommandLineRunner {
+
+	
+	
+	@Autowired
+	private CategoriaRepository categoriarepo;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBackendJavaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		 Categoria cat1 = new Categoria(null, "Informática");
+		 Categoria cat2 = new Categoria(null, "Eletrônico");
+		
+		 
+		 categoriarepo.saveAll(Arrays.asList(cat1,cat2));
+	}
+
+	
+	
 }
