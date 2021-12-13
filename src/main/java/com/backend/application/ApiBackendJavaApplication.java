47e93475fe1eb7f0fe5c9f21ad2c9f8e538f70be
marciogrecco <marcio.greco@outlook.com>
2021-12-13 08:50:33 -0300

    customer type customer phone address

--- src/main/java/com/backend/application/ApiBackendJavaApplication.java
+++ src/main/java/com/backend/application/ApiBackendJavaApplication.java
@@ -1,6 +1,5 @@
 package com.backend.application;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.backend.application.domain.Categoria;
 import com.backend.application.domain.Cidade;
+import com.backend.application.domain.Cliente;
+import com.backend.application.domain.Endereco;
 import com.backend.application.domain.Estado;
 import com.backend.application.domain.Produto;
+import com.backend.application.domain.enun.TipoCliente;
 import com.backend.application.domain.repository.CategoriaRepository;
 import com.backend.application.domain.repository.CidadeRepository;
+import com.backend.application.domain.repository.ClienteRepository;
+import com.backend.application.domain.repository.EnderecoRepository;
 import com.backend.application.domain.repository.EstadoRepository;
 import com.backend.application.domain.repository.ProdutoRepository;
 
@@ -30,6 +34,11 @@ public class ApiBackendJavaApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepo;
 	@Autowired
 	private CidadeRepository cidadeRepo;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBackendJavaApplication.class, args);
@@ -51,6 +60,15 @@ public class ApiBackendJavaApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
@@ -65,7 +83,10 @@ public class ApiBackendJavaApplication implements CommandLineRunner {
 		produtorepo.saveAll(Arrays.asList(p1, p2, p3));
 
 		estadoRepo.saveAll(Arrays.asList(est1, est2));
-       cidadeRepo.saveAll(Arrays.asList(c1,c2,c3));
+		cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+	
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+	
 	}
-
 }
---
+++ src/main/java/com/backend/application/domain/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.backend.application.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.backend.application.domain.Categoria;
+import com.backend.application.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
